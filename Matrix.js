e6ffc1dd425b6dcff12907a5af7429ee70f65aa0
Alec5 <alec.berger7@gmail.com>
2020-09-26 20:32:15 -0400

    Update Matrix.js

--- Matrix.js
+++ Matrix.js
@@ -1,10 +1,10 @@
 class Matrix
 {
-  constructor()
+  constructor(arr)
   {
-    this.matrix = [[0]];
-    this.m = this.matrix.length;
-    this.n = this.matrix[0].length;
+    this.matrix = arr;
+    this.m = arr.length;
+    this.n = arr[0].length;
   }
 
   setMatrix(arr)
@@ -14,8 +14,19 @@ class Matrix
     this.n = arr[0].length;
   }
 
+  duplicateMatrix()
+  {
+    var newArray = [];
+    for (var r = 0; r < this.m; r++)
+    {
+      newArray.push([]);
+      for (var c = 0; c < this.n; c++)
+        newArray[r][c] = this.matrix[r][c];
+    }
+    return new Matrix(newArray);
+  }
+
   // multiplies every entry in the matrix by amount
-  // precondition: amount is not 0
   scale(amount)
   {
     // call scaleRow on each row
@@ -50,40 +61,33 @@ class Matrix
   swapRows(row1, row2)
   {
     for (var c = 0; c < this.n; c++)
-    {
       this.swapEntries([row1, c], [row2, c]);
-    }
   }
 
   // add a multiple of a row to another row
   addRows(scalar, r1, r2)
   {
     for (var c = 0; c < this.n; c++)
-    {
       this.matrix[r2][c] = math.add(this.matrix[r2][c], math.multiply(this.matrix[r1][c], scalar));
-    }
   }
 
   // check if a row is all zeros
   isZeroRow(rowNum)
   {
     for (var c = 0; c < this.n; c++)
-    {
       if (this.matrix[rowNum][c] != 0)
         return false;
-    }
     return true;
   }
 
   // finds index of the leading variable in a row
+  // returns n if no leading variable is found, indicating a 0 row
   indexOfLeadingVar(rowNum)
   {
     for (var c = 0; c < this.n; c++)
-    {
       if (this.matrix[rowNum][c] != 0)
         return c;
-    }
-    return null;
+    return this.n;
   }
 
   // returns true if matrix is in REF
@@ -157,6 +161,219 @@ class Matrix
     return true;
   }
 
+  // returns a new matrix, which is the current matrix transposed
+  transpose()
+  {
+    var newArray = [];
+
+    // dimensions are inversed (new matrix has n rows and m columns)
+    for (var r = 0; r < this.n; r++)
+      newArray.push([]);
+
+    // loop through matrix in column-major order
+    // add each element to new matrix in row-major order
+    for (var c = 0; c < this.n; c++)
+    {
+      for (var r = 0; r < this.m; r++)
+        newArray[c][r] = this.matrix[r][c];
+    }
+    return new Matrix(newArray);
+  }
+
+  // helper method for finding determinant
+  // returns subset of matrix that excludes
+  // row i and column j
+  subset(i, j)
+  {
+    var newArray = [];
+
+    for (var r = 0; r < this.m - 1; r++)
+      newArray.push([]);
+
+    var newR = 0;
+    var newC = 0;
+
+    for (var r = 0; r < this.m; r++)
+    {
+      if (r != i)
+      {
+        newC = 0;
+        for (var c = 0; c < this.n; c++)
+        {
+          if (c != j)
+          {
+            newArray[newR][newC] = this.matrix[r][c];
+            newC++;
+          }
+        }
+        newR++;
+      }
+    }
+
+    return new Matrix(newArray);
+  }
+
+  // TODO: remake det() method to be more efficient
+  // calculates the determinant
+  // Precondition: m == n
+  det()
+  {
+    if (this.m == 1)
+      return this.matrix[0][0];
+
+    if (this.m == 2)
+    {
+      return math.subtract(
+               math.multiply(this.matrix[0][0], this.matrix[1][1]),
+               math.multiply(this.matrix[0][1], this.matrix[1][0])
+      );
+    }
+
+    var total = 0;
+    var sign = 1;
+
+    // loop through elements of first column
+    for (var r = 0; r < this.m; r++)
+    {
+      // calculate cofactor of each element: (alternating sign) * subset(r, 0).det()
+      var cofactor = math.multiply(sign, this.subset(r, 0).det());
+
+      // multiply each element in this column by the cofactor for that element
+      var ac = math.multiply(this.matrix[r][0], cofactor);
+
+      // add this to a total
+      total = math.add(total, ac);
+
+      sign *= -1;
+    }
+    return total;
+  }
+
+  // returns a new matrix which is the original matrix in RREF
+  calcRREF()
+  {
+    var newMatrix = this.duplicateMatrix();
+
+    var completeRows = 0;
+    // repeat until all rows are complete
+    while (completeRows < newMatrix.m)
+    {
+      // find the row with the leftmost leading variable
+      // by looping through incomplete rows, finding which has min leadingVar
+      var leadingIndex = newMatrix.indexOfLeadingVar(completeRows);
+      var leadingRow = completeRows;
+      var leadingVar = newMatrix.matrix[completeRows][leadingIndex];
+
+      for (var r = completeRows; r < newMatrix.m; r++)
+      {
+        var rowIndex = newMatrix.indexOfLeadingVar(r)
+        if (rowIndex < leadingIndex)
+        {
+          leadingIndex = rowIndex;
+          leadingRow = r;
+          leadingVar = newMatrix.matrix[r][leadingIndex];
+        }
+      }
+
+      // swap this row with the topmost incomplete row
+      if (leadingRow != completeRows)
+        newMatrix.swapRows(leadingRow, completeRows);
+
+      // multiply the row by 1/leadingVar
+      if (!newMatrix.isZeroRow(completeRows))
+      {
+        newMatrix.scaleRow(completeRows, math.divide(1, leadingVar));
+
+        // subtract (c * this row) from all other rows,
+        // where c = the coefficient in the same column as this row's leading variable
+        for (var r = 0; r < newMatrix.m; r++)
+          if (r != completeRows)
+            newMatrix.addRows(math.multiply(newMatrix.matrix[r][leadingIndex], -1), completeRows, r);
+      }
+
+      // increase the complete rows counter by one
+      completeRows++;
+    }
+    return newMatrix;
+  }
+
+  // returns a new matrix which is the original matrix inversed
+  // Precondition: matrix is square and determinant is not 0
+  inverse()
+  {
+    var newMatrix = this.duplicateMatrix();
+
+    // make an identity matrix with the same dimensions as this matrix
+    var identityArr = [];
+    for (var r = 0; r < a.m; r++)
+    {
+      identityArr.push([])
+      for (var c = 0; c < a.n; c++)
+      {
+        if (c == r)
+          identityArr[r][c] = 1;
+        else
+          identityArr[r][c] = 0;
+      }
+    }
+
+    // run the RREF algorithm and do all the row operations on the identity matrix simultaneously
+    var invertedMatrix = new Matrix(identityArr);
+
+    var completeRows = 0;
+    // repeat until all rows are complete
+    while (completeRows < newMatrix.m)
+    {
+      // find the row with the leftmost leading variable
+      // by looping through incomplete rows, finding which has min leadingVar
+      var leadingIndex = newMatrix.indexOfLeadingVar(completeRows);
+      var leadingRow = completeRows;
+      var leadingVar = newMatrix.matrix[completeRows][leadingIndex];
+
+      for (var r = completeRows; r < newMatrix.m; r++)
+      {
+        var rowIndex = newMatrix.indexOfLeadingVar(r)
+        if (rowIndex < leadingIndex)
+        {
+          leadingIndex = rowIndex;
+          leadingRow = r;
+          leadingVar = newMatrix.matrix[r][leadingIndex];
+        }
+      }
+
+      // swap this row with the topmost incomplete row
+      if (leadingRow != completeRows)
+      {
+        newMatrix.swapRows(leadingRow, completeRows);
+        invertedMatrix.swapRows(leadingRow, completeRows);
+      }
+
+      if (!newMatrix.isZeroRow(completeRows))
+      {
+        // multiply the row by 1/leadingVar
+        newMatrix.scaleRow(completeRows, math.divide(1, leadingVar));
+        invertedMatrix.scaleRow(completeRows, math.divide(1, leadingVar));
+
+        // subtract (c * this row) from all other rows,
+        // where c = the coefficient in the same column as this row's leading variable
+        for (var r = 0; r < newMatrix.m; r++)
+        {
+          if (r != completeRows)
+          {
+            var c = math.multiply(newMatrix.matrix[r][leadingIndex], -1);
+            newMatrix.addRows(c, completeRows, r);
+            invertedMatrix.addRows(c, completeRows, r);
+          }
+        }
+      }
+
+      // increase the complete rows counter by one
+      completeRows++;
+    }
+    return invertedMatrix;
+
+  }
+
   printToConsole()
   {
     var output = ""
@@ -172,6 +389,5 @@ class Matrix
     }
     console.log(output);
   }
-
 
 }
